1edad432dac748c7f03a8ce32846b49b186b0ae6
Tythan <tsaqiif26@gmail.com>
2024-11-29 08:54:44 +0700

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Mario!");
+        System.out.println("Hello, Mariooo!");
     }
 }
